ad6f733439420afedffbd39ac6d0b2f7fa47713d
Hassan767275 <hassanakhtar291@gmail.com>
2026-06-02 20:20:51 -0400

    got my frontend and backend talking

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,12 @@
 import './App.css'
+import { useEffect } from 'react'
 
 function App() {
+  useEffect(() => {
+    fetch('http://localhost:8000/api')
+      .then(res => res.json())
+      .then(data => console.log(`message recived ${data}`))
+  }, [])
   return (
     <>
       <h1>welcome to my githib repo project</h1>
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,15 @@
 import express from "express"
+import cors from "cors"
 
 const PORT = 8000
 
 const app = express()
+
+app.use(cors())
+
+app.get("/api", (req, res) => {
+    res.json("Hello from express")
+})
 
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
