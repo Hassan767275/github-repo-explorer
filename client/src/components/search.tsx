fee4b82768b0ad021787053500d6da89a3ae1ad7
Hassan767275 <hassanakhtar291@gmail.com>
2026-06-06 21:53:05 -0400

    when user serachs into search bar make a request to backend which will call the github api to return user repo info

--- client/src/components/search.tsx
+++ client/src/components/search.tsx
@@ -1,8 +1,21 @@
 export default function Search() {
-    return (
-        <div className="m-4 text-center">
-            <input className="text-[#e6edf3] bg-[#161b22] border-[#30363d] rounded-lg mr-4 pl-4 py-1" type='text' placeholder="search a github repo"></input>
-            <button className="bg-[#238636] text-[#e6edf3] px-4 py-1 rounded-lg font-bold" >Search</button>
-        </div>
-    )
+    async function searchRepos(e: any) {
+        e.preventDefault()
+        const data = await fetch('http://localhost:8000/search')
+        const dataJson = await data.json()
+        console.log(dataJson)
+    }
+
+  return (
+    <form className="m-4 flex justify-center" onSubmit={searchRepos}>
+      <input
+        className="text-[#e6edf3] bg-[#161b22] border-[#30363d] rounded-lg mr-4 pl-4 py-1 sm:w-80 md:w-110 lg:w-150"
+        type="text"
+        placeholder="search a github repo"
+      ></input>
+      <button className="bg-[#238636] text-[#e6edf3] px-4 py-1 rounded-lg font-bold">
+        Search
+      </button>
+    </form>
+  );
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -11,6 +11,13 @@ app.get("/api", (req, res) => {
     res.json("Hello from express")
 })
 
+app.get("/search", async (req, res) => {
+    const repo = await fetch(`https://api.github.com/users/${'Hassan767275'}/repos`)
+    const repoJson = await repo.json()
+    res.json(repoJson)
+
+})
+
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}`)
 })
